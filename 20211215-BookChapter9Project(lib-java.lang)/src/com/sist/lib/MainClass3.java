dbdd61928cb9070c1ed0c7eaceb6b68abdf74193
kimin <kimin@DESKTOP-SIBS35J>
2021-12-15 21:39:16 +0900

    2021-12-15라이브러리

--- 20211215-BookChapter9Project(lib-java.lang)/src/com/sist/lib/MainClass3.java
+++ 20211215-BookChapter9Project(lib-java.lang)/src/com/sist/lib/MainClass3.java
@@ -1,5 +1,22 @@
 package com.sist.lib;
-
+// indexOf 
+// lastIndexOf 
 public class MainClass3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        String s="Hello Java!!";
+        //        01234567891011
+        int i=s.indexOf('a');
+        System.out.println("i="+i);
+        int j=s.lastIndexOf('a');
+        System.out.println("j="+j);
+        
+        s="c:\\javaDev\\java.basic\\Hello.java";
+        String ss=s.substring(s.lastIndexOf(".")+1);
+        System.out.println(ss);
+        
+        		
+	}
 
 }
