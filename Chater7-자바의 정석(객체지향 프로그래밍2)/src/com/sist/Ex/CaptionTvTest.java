8c0798eb035f7f52ebb73eeb86adefa1fc2d326a
kiminsoo114 <kiminsu114@hanmail.net>
2022-01-01 23:43:17 +0900

    자바의 정석 chapter7

---
+++ Chater7-자바의 정석(객체지향 프로그래밍2)/src/com/sist/Ex/CaptionTvTest.java
@@ -0,0 +1,42 @@
+package com.sist.Ex;
+
+class Tv {
+	boolean power;// 전원 상태(on/off)
+	int channel; // 채널
+
+	void power() {
+		power = !power;
+	}
+
+	void channelUp() {
+		++channel;
+	}
+
+	void channelDown() {
+		--channel;
+	}
+}
+
+class CaptionTv extends Tv {
+	boolean caption; // 캡션상태(on/off)
+
+	void dispalyCaption(String text) {
+		if (caption) {// 캡션 상태가 on(true)일 때만 text를 보여준다.
+			System.out.println(text);
+
+		}
+	}
+}
+
+public class CaptionTvTest {
+	public static void main(String[] args) {
+		CaptionTv ctv = new CaptionTv();
+		ctv.channel = 10; // 조상 클래스로부터 상속받은 멤버
+		ctv.channelUp(); // 조상 클래스로부터 상속받은 멤버
+		System.out.println(ctv.channel);
+		ctv.dispalyCaption("Hello, World");
+		ctv.caption = true; // 캡션(자막)기능을 켠다
+		ctv.dispalyCaption("Hello, World");
+	}
+
+}
