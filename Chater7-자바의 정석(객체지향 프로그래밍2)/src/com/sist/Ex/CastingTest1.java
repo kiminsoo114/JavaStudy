df4fa44c06f4ba02bf1910be09427090e172634c
kiminsoo114 <kiminsu114@hanmail.net>
2022-01-11 23:31:51 +0900

    Chapter7 java

--- Chater7-자바의 정석(객체지향 프로그래밍2)/src/com/sist/Ex/CastingTest1.java
+++ Chater7-자바의 정석(객체지향 프로그래밍2)/src/com/sist/Ex/CastingTest1.java
@@ -9,10 +9,12 @@ public class CastingTest1 {
 		fe.water();
 		tru = fe; // tru = (tru)fe;에서 형변환이 생략된 형태
 		fe2 = (FireEngine) tru;// 자손타입<-조상타입
+		tru.stop();
 		fe2.water();
 		tru.drive();
 		fe2.stop();
 		fe.stop();
+		
 
 	}
 }
